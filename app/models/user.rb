6c8e6abd5a85b47f35aaf6b3d8ba87ecdd8d2a97
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-30 18:41:22 +0900

    Fix: user's password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, format: { with: /\A[A-z0-9]+\z/ }
 
 
 end
