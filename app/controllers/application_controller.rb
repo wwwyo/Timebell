083f8d77a9bc53c9dfdd8fef8eee1acd4b248ecf
yuito118 <72675978+yuito118@users.noreply.github.com>
2020-10-30 14:28:13 +0900

    Fix: devise redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+  
+  def after_sign_in_path_for(resource)
+    schedules_path
+  end
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
